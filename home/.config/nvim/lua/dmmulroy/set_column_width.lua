88233aced6aa734e001a7e227a064b4fd408be13
Raimonds Korzenevskis <raimonds.korzenevskis@gmail.com>
2026-02-05 11:46:01 +0200

    sync

--- home/.config/nvim/lua/dmmulroy/set_column_width.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_user_command("SetColumnWidth80", function()
-	vim.opt.colorcolumn = "80"
-end, {})
-
-vim.api.nvim_create_user_command("SetColumnWidth100", function()
-	vim.opt.colorcolumn = "100"
-end, {})
