f02153ab99ea5ba04dfc8b8004b33fcc945bed22
Raimonds Korzenevskis <raimonds.korzenevskis@gmail.com>
2026-03-15 16:56:46 +0200

    dotfiles updates

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,25 +1,3 @@
-# Disable greeting
-set fish_greeting 
-
-# Set Editor to neovim
-set -gx EDITOR 'nvim'
-
-# Set neovim as the program to open manpages
-set -gx MANPAGER 'nvim +Man!'
-
-# Add dotfiles directory to PATH for 'dot' command
-fish_add_path ~/.dotfiles
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
-
-# opencode
-fish_add_path /Users/raimondskorzenevskis/.opencode/bin
-
-# pnpm
-set -gx PNPM_HOME "/Users/raimondskorzenevskis/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+if status is-interactive
+# Commands to run in interactive sessions can go here
 end
-# pnpm end
